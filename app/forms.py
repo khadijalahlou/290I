8ef710848bb726d6bdd95b0e43ef0bbe473dbdea
khadijalahlou <khadija.lahlou@berkeley.edu>
2015-12-01 11:41:33 -0800

    4rt commit

---
+++ app/forms.py
@@ -0,0 +1,10 @@
+from wtforms import Form, StringField, BooleanField
+from wtforms.validators import DataRequired
+
+class LoginForm(Form):
+    openid = StringField('openid', validators=[DataRequired()])
+    remember_me = BooleanField('remember_me', default=False)
+
+#The DataRequired import is a validator, a function that can be attached 
+#to a field to perform validation on the data submitted by the user. The 
+#DataRequired validator simply checks that the field is not submitted empty
